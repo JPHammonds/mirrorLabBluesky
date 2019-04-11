35e245aa251154f1431706e21e3c6a62ee6f5520
John Hammonds <JPHammonds@anl.gov>
2019-04-11 00:28:42 -0500

    Cleanup and adding/refining comments and thoughts for the future.

--- startup/52-multilayer-plan.py
+++ startup/52-multilayer-plan.py
@@ -15,19 +15,40 @@ test_layers = [
 #      'between_layer_time':5, 'number_of_passes':1, 'description': 'B4C'}
     ]
 
+# These load methods need some more work.  In the long run, would loke to be 
+# able tp define the patterns in a script or in something like a json or yaml
+# file initially but configurations which are read this way would be stored in
+# a database with some reference ID, so that it can be referred to later.
 def load_pattern_from_db(pattern_name):
+    '''
+    Assuming we get patterns into a db, with some ID as a key, pull the pattern
+    associated with ID=pattern_name
+    '''
     pass
     
 def load_pattern_from_file(pattern_name):
+    '''
+    Load pattern from a file.  Should use some common format, json, yaml, ...
+    Final format has not been defined.
+    
+    '''
     pass
 
 def load_pattern_default():
+    '''
+    Initially we are using a defined pattern, stored in a dictionry.  This 
+    provides some simplicity now.  Don't be rigid enough that this causes 
+    problems later  
+    '''
     return test_layers
 
 def multi_layer(pattern_name = 'name', from_file=False, pattern_repeat = 1):
     
-    """pa
+    """
     Set up to deposit multiple instances of a layering sequence
+     This is the main script at this point.  To launch this, yow would type:
+     RE(multi_layer(XXX))
+     proper parameters should replace XXX 
     """
     def get_pattern(pattern_name):
 
@@ -41,11 +62,21 @@ def multi_layer(pattern_name = 'name', from_file=False, pattern_repeat = 1):
         return pattern
     
     def process_multilayer(layer_num, layer):
+        '''
+            Take steps to coat a layer.  Separated into pre, do and post
+            methods
+        '''
         yield from pre_layer(layer)
         yield from do_mono_layer(layer)
         yield from post_layer(layer)
         
     def process_all_layers():
+        '''
+            Take care of the main coordinating the main steps of layers.
+        overall creates n iterations of the loaded layer pattern.  The pattern 
+        has multiple layers of multiple materials.  For each materal, controlled
+        by changing guns, speed, number of layers, etc is settable.
+        '''
         for i in range(pattern_repeat):
 #             _md['multi_layer'][str(i+1)] = {}
             sub_layer=1
@@ -56,7 +87,7 @@ def multi_layer(pattern_name = 'name', from_file=False, pattern_repeat = 1):
                 yield from process_multilayer(i+1, layer)
                 sub_layer += 1
 #             _md['end_time'] = time.ctime()
-    
+    # Overall execution starts here,  reference back to the other inner methods
     pattern = get_pattern(pattern_name)        
     yield from process_all_layers()
     
@@ -67,8 +98,13 @@ def multi_layer(pattern_name = 'name', from_file=False, pattern_repeat = 1):
 #     return (yield from write_meta_data())
 
 def pre_layer( layer):
-#     pass
+    ''' 
+    Setup work to be done before doing the actual coating(mobe to starting
+    position, prep the gun, ...
+    '''
     def _prepare_layer():
+        # bps.mv is basically a set.  pvs are treated like motors since that
+        # was the first thing scanned
         yield from bps.mv(depos_sys.m1.velocity, layer['speed'])
         yield from bps.mv(depos_sys.m1, layer['start_position'])
         yield from bps.mv(depos_sys.gun_selector, layer['selected_gun'])
@@ -78,9 +114,20 @@ def pre_layer( layer):
               (depos_sys.m1.name, depos_sys.m1.velocity.get()))
     
         return bps.sleep(layer['between_layer_time'])
+    # Should try removing the return and just yield.  
     return (yield from _prepare_layer())
     
 def do_mono_layer(layer):
+    '''
+    Actual coating done here.  When you get here you should be ready to go.
+    Settings come from the pattern definition.
+       - Enable the gun
+       - loop for number of passes for the monolayer
+          * start moving to end position
+          * sleep between layers
+          * Move back to the start position
+    Some things will change here as we use the info in the gun to do the move
+    '''
     logging.info("Starting Mono Layer %s" % layer['description'])
     yield from bps.mv(depos_sys.gun_selector.mps1_enable_output, 1)
     for l in range(layer['number_of_passes']):
@@ -89,6 +136,11 @@ def do_mono_layer(layer):
         yield from bps.mv(layer['motor'], layer['start_position'])
         
 def post_layer(layer):
+    '''
+    When  a layer is done clean up.  For now disable the gun (i.e. set the 
+    gun selector to zero.  If this does not get more complex. can get rid of 
+    do_post_layer, or pop it up an get rid of post_layer.
+    '''
     def do_post_layer():
         yield from bps.mv(depos_sys.gun_selector, \
                           depos_sys.gun_selector.GUN_DISABLE_VAL)
