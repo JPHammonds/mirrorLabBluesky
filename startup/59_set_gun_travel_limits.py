15a3744432d8e973764a77cf2ef9983599f70569
John Hammonds <JPHammonds@anl.gov>
2019-04-17 11:24:02 -0500

    Add doc string to describe the use of this file.

--- startup/59_set_gun_travel_limits.py
+++ startup/59_set_gun_travel_limits.py
@@ -1,3 +1,8 @@
+'''
+This file sets gun parameters used in calculating positions that do not change
+very often.  These should be parameters that only change when something chang
+in instrumentation on the deposition system.
+'''
 depos_sys.gun_selector.guns.gun1.set_travel_limits(mask_width=40, zero_position=1111.2, overspray=45)
 depos_sys.gun_selector.guns.gun2.set_travel_limits(mask_width=40, zero_position=1318.4, overspray=45)
 depos_sys.gun_selector.guns.gun3.set_travel_limits(mask_width=40, zero_position=1517.6, overspray=45)
